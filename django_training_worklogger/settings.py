86e52d6b7d0aa1877bc8f6e99551f90d75991b9d
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-12-04 18:40:24 +0200

    Use `django-environ`

--- django_training_worklogger/settings.py
+++ django_training_worklogger/settings.py
@@ -10,26 +10,41 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p-j%o5j@8xei^x_+zlwc+#zgzew-xpkf662p6n525^!nv5n5zs"
+# SECRET_KEY = "django-insecure-p-j%o5j@8xei^x_+zlwc+#zgzew-xpkf662p6n525^!nv5n5zs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 # To test 404 page
-DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
