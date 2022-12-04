6fabf25d55e9e149af5231e62789ab3b2e440ce9
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-12-04 18:37:16 +0200

    Add `list_display`

--- logger/admin.py
+++ logger/admin.py
@@ -4,6 +4,7 @@ from .models import Log, Project
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'get_total_logged_hours',]
     def get_total_logged_hours(self, obj):
         result = obj.log_set.all().aggregate(total=Sum("duration"))
         return result["total"]
